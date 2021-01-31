0902dd56cf3dee0a0650018cad7922c3971b28ce
KolpashikovDenis <kolpashikov.denis@gmail.com>
2021-01-31 22:34:07 +0400

    Файлы переименованы

--- create_equipment.py
+++ create_equipment.py
@@ -12,17 +12,17 @@ config.read(configfilename)
 hostname = config['DEFAULT']['hostname']
 login = config['DEFAULT']['login']
 password = config['DEFAULT']['password']
-str_date = config['DEFAULT']['date']
 input_file = config['DEFAULT']['inputfile']
-is_entity = bool(config['DEFAULT']['entity'])
-is_entity_route_sheet_transaction = bool(config['DEFAULT']['entity_route_sheet_transaction'])
-is_entity_route_sheet = bool(config['DEFAULT']['entity_route_sheet'])
-is_entity_route_sheet_operation = bool(config['DEFAULT']['entity_route_sheet_operation'])
-is_operation = bool(config['DEFAULT']['operation'])
-is_department = bool(config['DEFAULT']['department'])
-is_equipment_class = bool(config['DEFAULT']['equipment_class'])
-is_entity_batch = bool(config['DEFAULT']['entity_batch'])
-is_user = bool(config['DEFAULT']['user'])
+# str_date = config['DEFAULT']['date']
+# is_entity = bool(config['DEFAULT']['entity'])
+# is_entity_route_sheet_transaction = bool(config['DEFAULT']['entity_route_sheet_transaction'])
+# is_entity_route_sheet = bool(config['DEFAULT']['entity_route_sheet'])
+# is_entity_route_sheet_operation = bool(config['DEFAULT']['entity_route_sheet_operation'])
+# is_operation = bool(config['DEFAULT']['operation'])
+# is_department = bool(config['DEFAULT']['department'])
+# is_equipment_class = bool(config['DEFAULT']['equipment_class'])
+# is_entity_batch = bool(config['DEFAULT']['entity_batch'])
+# is_user = bool(config['DEFAULT']['user'])
 
 auth_data = "{\"action\":\"login\",\"data\":{\"login\":\""+login+"\",\"password\":\""+password+"\"}}"
 template_body = "{\"data\":{\"identity\":\"%s\",\"name\":\"%s\",\"equipment_class_id\":%s,\"department_id\":%s}}"
@@ -45,5 +45,6 @@ if __name__ == '__main__':
         body = body.encode('UTF-8')
         response = req.post(hostname + '/action/equipment/create', data = body, cookies = c, headers = h)
         print(response.content)
-        print()
-        pass
+
+responce = req.get(hostname+'/action/logout', cookies=c, headers=h)
+print('Done.')
